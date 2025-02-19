ec5b28ec7b85fd2208485162dd51438b58987aaf
Kox-Hell <jcdeleonozuna@gmail.com>
2025-02-18 19:28:28 -0600

    cookies

--- cookies-popUp/styles.js
+++ cookies-popUp/styles.js
@@ -1,6 +1,22 @@
 let closeButton = document.querySelector('.button-close');
 let likeButton = document.querySelector('.button-like')
 
+let tabs = document.querySelectorAll('.tab');
+let content = document.querySelectorAll('.content-tab-container');
+
+tabs.forEach(function(tab) {
+    tab.addEventListener('click', function () {
+        tabs.forEach(function(t) {
+            t.classList.remove('active');
+        });
+        content.forEach(function(c) {
+            c.classList.remove('active');
+        });
+        tab.classList.add('active');
+        let contentId = tab.id.replace('tab', 'content');
+        document.getElementById(contentId).classList.add('active');
+    });
+});
 document.addEventListener('DOMContentLoaded', function(event) {
 
     let cookieContainer = document.querySelector('.cookies-container');
